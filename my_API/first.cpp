89bd5691215f28022512d6912971db4518e7703a
fecigindev <33804459+fecigindev@users.noreply.github.com>
2019-04-22 13:36:30 -0700

    Create first.cpp

---
+++ my_API/first.cpp
@@ -0,0 +1,37 @@
+#include <SDL.h>
+#include <stdio.h>
+
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main( int argc, char* args[] )
+{
+	//The window we'll be rendering to
+	SDL_Window* window = NULL;
+
+	//The surface contained by the window
+	SDL_Surface* screenSurface = NULL;
+
+    //Create window
+    window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+
+    screenSurface = SDL_GetWindowSurface( window );
+
+    //Fill the surface white
+    SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+
+    //Update the surface
+    SDL_UpdateWindowSurface( window );
+
+    //Wait two seconds
+    SDL_Delay( 2000 );
+
+	//Destroy window
+	SDL_DestroyWindow( window );
+
+	//Quit SDL subsystems
+	SDL_Quit();
+
+	return 0;
+}
