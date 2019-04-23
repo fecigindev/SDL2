f522bd57063befb3ab2ee0c9bd64793b97005b49
fecigindev <33804459+fecigindev@users.noreply.github.com>
2019-04-23 03:10:51 -0700

    Create loadmedia.h

---
+++ my_API/2_loadBMP/loadmedia.h
@@ -0,0 +1,18 @@
+© 2019
+
+bool loadMedia()
+{
+	//load media success
+	bool success = true;
+
+	//Load splash image
+	gXOut = SDL_LoadBMP( "x.bmp" );
+	if( gXOut == NULL )
+	{
+		printf( " image tidak bisa diload ke aplikasi %s! SDL Error: %s\n",
+         "03_event_driven_programming/x.bmp", SDL_GetError() );
+		success = false;
+	}
+
+	return success;
+}
